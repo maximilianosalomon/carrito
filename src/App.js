c32c5ca5e9683c3ff14fa5aee4198fd26901d064
Maximiliano Salomon <maxi.salomon@gmail.com>
2021-11-01 22:05:27 -0300

    clase 3

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 // Original
-import logo from "./logo.svg";
 import "./App.css";
 
-// Maxi
-import Navbar from "./components/Navbar";
-import { AppBar } from "@mui/material";
+// a
+import Navbar from "./components/navbar/navbar";
+
 //Funcion
 function App() {
   return (
     <header>
-      <Navbar />
-      <div>hola!</div>
+      <div className="App">
+        <Navbar />
+      </div>
+      <div>Hola!</div>
     </header>
   );
 }
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -3,9 +3,9 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
+import ListaCategorias from "../list/lista";
 
 export default function ButtonAppBar() {
   return (
@@ -22,9 +22,9 @@ export default function ButtonAppBar() {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+            Mi Tienda
           </Typography>
-          <Button color="inherit">Login</Button>
+          <ListaCategorias />
         </Toolbar>
       </AppBar>
     </Box>
