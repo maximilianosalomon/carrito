7b96fbe88c7fb1750e7f6300c9821dacd3b16e28
Maximiliano Salomon <maxi.salomon@gmail.com>
2021-12-03 01:28:54 -0300

    se agregan cambios en item y item detail

--- src/App.js
+++ src/App.js
@@ -5,18 +5,19 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import ItemListContainer from "./components/ItemListContainer";
 import ItemDetailContainer from "./components/ItemDetailContainer";
+import About from "./components/About";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Routes>
+        <Route path="item/:id" element={<ItemDetailContainer />} />
         <Route
           path="/"
           element={<ItemListContainer greeting="Bienvenidos" />}
         />
-        <Route path="item/:id" element={<ItemDetailContainer />} />
-        <Route />
+        <Route path="about" element={<About />} />
       </Routes>
     </div>
   );
